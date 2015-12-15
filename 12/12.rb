90f8755ae45904e15c530fe3c130556094aa2c62
Michal Cichra <michal@o2h.cz>
2015-12-15 21:08:49 +0100

    12th advent of code

--- 12/12.rb
+++ 12/12.rb
@@ -1,61 +1,22 @@
-require 'json/stream'
 require 'json'
 
-def ARGF.close; end
-def ARGF.force_encoding(*); end
-def ARGF.valid_encoding?(*); true; end
+document = JSON.parse(ARGF.read)
 
-class Builder < JSON::Stream::Builder
-  attr_reader :total
-
-  def initialize(*)
-    super
-    @numbers = []
-    @total = []
-    @ignore = nil
-    @objects = []
-  end
-
-  def start_object
-    @numbers.push([])
-    @objects.push(true)
-    super
-  end
-
-  def end_object
-    return if @stack.size == 1
-
-    @total.push @numbers.pop
-
-    if !(ignore = @objects.pop)
-      puts "IGNORING #{@stack.pop}"
+numbers = []
+recursion = ->(object) do
+  r = recursion.method(:call)
+  case object
+    when Hash
+      values = object.values
+      values.flat_map(&r) unless values.include?('red'.freeze)
+    when Array
+      object.flat_map(&r)
+    when Numeric
+      numbers << object
+      object
     else
-      super
-    end
-  end
-
-  def value(val)
-    case val
-      when 'red'
-        if @stack[-1].is_a?(Hash)
-          @objects[-1] = false
-        end
-      when Numeric
-        @numbers[-1] << val
-    end
-
-    super
+      nil
   end
 end
 
-parser = JSON::Stream::Parser.new
-
-builder = Builder.new(parser)
-
-parser << ARGF
-
-p builder.result
-
-puts JSON.pretty_generate(builder.result)
-p builder.total.flatten.reduce(:+)
-
+p document.flat_map(&recursion).compact.reduce(:+)
